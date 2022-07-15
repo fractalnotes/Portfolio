1abfe53d8aae92d7ddfba18144e87682c682462c
fractalnotes <mlallanab01@gmail.com>
2022-07-15 14:37:28 +0200

    form

--- forms/contact.php
+++ forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'mlallanab01@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = './../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -23,14 +23,15 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
- 
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
-    'host' => 'smtp-relay.gmail.com',
+    'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
