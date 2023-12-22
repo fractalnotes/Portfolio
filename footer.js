0bc759847f858058cc23e73604c4db11a79ecddb
fractalnotes <mlallanab01@gmail.com>
2023-12-22 18:06:24 +0100

    New version

---
+++ footer.js
@@ -0,0 +1,18 @@
+document.getElementById('footer-container').innerHTML =`
+<!-- ======= Footer ======= -->
+<footer class="footer" role="contentinfo">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        
+      </div>
+      <div class="col-sm-12 col-12 social text-md-center">
+        <a href="https://www.instagram.com/manuel_l01/?hl=es" target="_blank"><span class="bi bi-instagram"></span></a>
+        <a href="https://mail.google.com/mail/?view=cm&source=mailto&to=mlallanab01@gmail.com" target="_blank"><span class="bi bi-envelope"></span></a>
+        </div>
+    </div>
+  </div>
+</footer>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+`;
